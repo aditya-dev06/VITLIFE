6c208e2562bd2972f1b53f8c95be3c0e9afc2d7b
aditya-dev06 <adityapoke76@gmail.com>
2026-06-23 01:45:24 +0530

    fix: Bump sw cache version to v3 for branding updates

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'cds-hub-static-cache-v2';
-const API_CACHE_NAME = 'cds-hub-api-cache-v2';
+const CACHE_NAME = 'cds-hub-static-cache-v3';
+const API_CACHE_NAME = 'cds-hub-api-cache-v3';
 
 const STATIC_ASSETS = [
   '/',
